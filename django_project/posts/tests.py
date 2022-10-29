341cbccc93732293dcc76733d9c5cd98ff51c9ee
jriverflow <k-mevol@hanmail.net>
2022-10-29 23:48:48 +0900

    heroku deployment2

--- django_project/posts/tests.py
+++ django_project/posts/tests.py
@@ -12,15 +12,21 @@ class PostTests(TestCase):
     def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test!")
 
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url_available_by_name(self):
+    def test_homepage(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
-    
-    def test_template_name_correct(self):
-        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
         self.assertContains(response, "This is a test!")
-        
+
+    # def test_url_exists_at_correct_location(self):
+    #     response = self.client.get("/")
+    #     self.assertEqual(response.status_code, 200)
+
+    # def test_url_available_by_name(self):
+    #     response = self.client.get(reverse("home"))
+    #     self.assertEqual(response.status_code, 200)
+    
+    # def test_template_name_correct(self):
+    #     response = self.client.get(reverse("home"))
+    #     self.assertContains(response, "This is a test!")
+
